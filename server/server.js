070ca085b4714850fbf4d9f7cb0199545c08f683
Younus Porteous <younusporteous@gmail.com>
2018-12-05 11:32:01 +0000

    added makeBid and challenge events

--- server/server.js
+++ server/server.js
@@ -23,19 +23,19 @@ io.on('connection', socket => {
   // TODO: add parameter to socket that identifies the game
   // local variable, or can we add to socket object directly?
   let currentGame
-  let player
+  let playerMe
 
   console.log('New player connected')
 
   // creating a new game; sign in at this time
-  socket.on('createGame', (params, callback) => {
+  socket.on('createGame', (params) => {
 
     // create a new game id
     let gameID = `${games.length}`.padStart(4, '0')
-    player = new Player(socket.id, params.displayName)
+    playerMe = new Player(socket.id, params.displayName)
 
     // create a game with that ID, and the current player's socket id
-    currentGame = new Game(gameID, [player])
+    currentGame = new Game(gameID, [playerMe])
     games.push(currentGame)
 
     // join that game's room as a player
@@ -44,32 +44,61 @@ io.on('connection', socket => {
     // report id to user to give to other potential players
     socket.emit('gameJoined', gameID)
     console.log(`created game ${gameID}`)
+
+    socket.on('startGame', () => {
+      // send currentPlayer to all players
+      io.to(currentGame.gameID).emit('newTurn', currentGame.currentPlayer)
+    })
   })
 
   // joining an existing game
-  socket.on('joinGame', (params, callback) => {
+  socket.on('joinGame', (params) => {
 
     // find room
     let room = io.sockets.adapter.rooms[params.gameID]
 
     // create player
-    player = new Player(socket.id, params.displayName)
+    playerMe = new Player(socket.id, params.displayName)
 
     // if room exists:
     if (room) {
       // game should exist if room exists, but add extra validation
       currentGame = games.filter(game => game.gameID === params.gameID)[0]
       // add player to game
-      currentGame.addPlayer(player)
+      currentGame.addPlayer(playerMe)
       // join room
       socket.join(params.gameID)
       // confirm with player
-      socket.emit('gameJoined', params.gameID)
+      let packet = {
+        gameID: params.gameID,
+        players: currentGame.players
+      }
+      socket.emit('gameJoined', packet)
+      io.to(currentGame.gameID).emit('updatePlayerList', currentGame.players)
     } else {
       // report that game does not exist
       socket.emit('gameNonExistent')
     }
   })
+
+  // playing the game
+  socket.on('makeBid', bid => {
+    // Game object itself checks that it is the right player's turn
+    currentGame.makeBid(playerMe, bid)
+    io.to(currentGame.gameID).emit('bidMade', bid)
+  })
+
+  socket.on('challenge', () => {
+    currentGame.challenge(playerMe, (bidWasCorrect, losingPlayer) => {
+      if (bidWasCorrect) {
+        io.to(currentGame.gameID).emit('challengeFailed', losingPlayer)
+      } else {
+        io.to(currentGame.gameID).emit('challengeSucceeded', losingPlayer)
+      }
+    })
+  })
+
+
 })
 
 
